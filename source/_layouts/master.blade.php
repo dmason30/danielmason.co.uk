a3fec84746080e8b1fe887b3f219408a3837b4b4
dmason30 <danmason@tmcapplications.co.uk>
2018-12-02 02:43:10 +0000

    seo

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="A methodical and efficient PHP developer that takes a logical approach to problems and produces a quality and optimized solution.">
+        <meta name="og:title" content="Daniel Mason - Web Developer">
+        <meta property="og:description" content="A methodical and efficient PHP developer that takes a logical approach to problems and produces a quality and optimized solution.">
+        <meta property="og:site_name" content="Daniel Mason - Web Developer">
+        <meta property="og:image" content="images/avatar.jpg">
+        <meta property="og:image:secure_url" content="images/avatar.jpg">
+        <meta name="twitter:description" content="A methodical and efficient PHP developer that takes a logical approach to problems and produces a quality and optimized solution.">
+        <meta name="twitter:title" content="Daniel Mason - Web Developer">
+        <meta name="twitter:title" content="Daniel Mason - Web Developer">
+        <meta name="twitter:image" content="images/avatar.jpg">
+        <title>Daniel Mason</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body class="bg-blue-dark text-black">
--- source/index.blade.php
+++ source/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="text-center">
                             <img
                                 class="h-32 mx-auto block rounded-full shadow-lg"
-                                src="//avatars0.githubusercontent.com/u/20278756?s=460&v=4"
+                                src="images/avatar.jpg"
                                 alt="Daniel Mason"
                             />
                             <span class="block mt-6 text-4xl font-light uppercase tracking-wide text-grey-darkest">
@@ -21,24 +21,24 @@
                             <span class="block mt-3 text-xl font-light text-grey-darkest">
                                 @
                             </span><span class="block mt-3 text-xl font-light text-grey-darkest">
-                                <a href="https://www.travelmoneyclub.co.uk">
+                                <a href="https://www.travelmoneyclub.co.uk" title="Travel Money Club">
                                     <img class="h-12" src="images/TMC_Logo.png" alt="Travel Money Club" />
                                 </a>
                             </span>
                             <div class="flex mt-8">
                                 <div class="w-1/3">
-                                    <a href="//twitter.com/danmasonmp">
-                                        <img class="h-12" src="svg/Twitter_Logo_Blue.svg" alt="twitter" />
+                                    <a href="//twitter.com/danmasonmp"  title="Twitter">
+                                        <img class="h-12" src="svg/Twitter_Logo_Blue.svg" alt="Twitter" />
                                     </a>
                                 </div>
                                 <div class="w-1/3">
-                                    <a href="//github.com/dmason30">
-                                        <img class="h-12" src="images/Octocat.png" alt="github" />
+                                    <a href="//github.com/dmason30" title="GitHub">
+                                        <img class="h-12" src="images/Octocat.png" alt="GitHub" />
                                     </a>
                                 </div>
                                 <div class="w-1/3">
-                                    <a href="//linkedin.com/in/masondaniel">
-                                        <img class="h-12" src="images/linkedin.png" alt="" />
+                                    <a href="//linkedin.com/in/masondaniel" title="LinkedIn">
+                                        <img class="h-12" src="images/linkedin.png" alt="LinkedIn" />
                                     </a>
                                 </div>
                             </div>
