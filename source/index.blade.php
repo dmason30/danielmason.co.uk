76eee2371517581f46011f29bcce5fd9d5772069
dmason30 <danmason@tmcapplications.co.uk>
2018-12-02 00:52:27 +0000

    initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,47 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="container mx-auto px-4 py-8">
+        <div class="sm:mt-32">
+            <div class="flex justify-center sm:mt-32">
+                <div class="w-full lg:w-1/2">
+                    <div class="bg-white shadow-lg p-6 rounded">
+                        <div class="text-center">
+                            <img class="h-32 mx-auto block rounded-full" src="//avatars0.githubusercontent.com/u/20278756?s=460&v=4" alt="Èrik Campobadal">
+                            <span class="block mt-6 text-4xl font-light uppercase tracking-wide text-grey-darkest">
+                                Daniel Mason
+                            </span>
+                            <span class="block mt-3 text-lg font-light text-grey-darkest">
+                                Senior Software Engineer
+                            </span>
+                            <span class="block mt-3 text-xl font-light text-grey-darkest">
+                                @
+                            </span><span class="block mt-3 text-xl font-light text-grey-darkest">
+                                <a href="http:://www.travelmoneyclub.co.uk">
+                                    <img class="h-12" src="images/TMC_Logo.png" alt="">
+                                </a>
+                            </span>
+                            <div class="flex mt-8">
+                                <div class="w-1/3">
+                                    <a href="//twitter.com/danmasonmp">
+                                        <img class="h-12" src="svg/Twitter_Logo_Blue.svg" alt="twitter">
+                                    </a>
+                                </div>
+                                <div class="w-1/3">
+                                    <a href="//github.com/dmason30">
+                                        <img class="h-12" src="images/Octocat.png" alt="github">
+                                    </a>
+                                </div>
+                                <div class="w-1/3">
+                                    <a href="//linkedin.com/in/masondaniel">
+                                        <img class="h-12" src="images/linkedin.png" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
