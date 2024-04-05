d15de0a0bf38a333f84b82de35cb79ffd5c147ae
dmason30 <danmason@spendology.com>
2024-04-05 03:56:52 +0100

    wip

--- resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
+++ resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
@@ -1,12 +1,7 @@
 <a
     href="{{ Routes::get("index") }}"
-    class="h-12 -rotate-6 transform transition duration-200 hover:rotate-0 hover:scale-110 md:h-32"
+    class="h-12 w-12 -rotate-6 transform transition duration-200 hover:rotate-0 hover:scale-110 md:h-32 md:w-32"
     aria-label="Home page"
 >
-    <img
-        src="/assets/logo.png"
-        title="Dan Mason Logo"
-        class="h-full"
-        loading="lazy"
-    />
+    <x-logo />
 </a>
